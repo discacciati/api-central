e507808bba9131279d52e9dd4d1251d6b3cb3656
Eliane Discacciati <discacciati@gmail.com>
2023-02-01 19:26:06 -0300

    Feat/ Estrutura inicial do projeto

---
+++ src/main/java/com/ordemcompra/apicentral/ApiCentralApplication.java
@@ -0,0 +1,13 @@
+package com.ordemcompra.apicentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCentralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ordemcompra/apicentral/ApiCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordemcompra.apicentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

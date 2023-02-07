a99c5f00ec7b2a294e41e70b504c3f468bcd4339
Eliane Discacciati <discacciati@gmail.com>
2023-02-06 23:26:44 -0300

    Fix: application properties

--- calculocotacao/src/main/java/com/ordem/compra/calculocotacao/CalculoCotacaoApiClient.java
+++ calculocotacao/src/main/java/com/ordem/compra/calculocotacao/CalculoCotacaoApiClient.java
@@ -2,11 +2,11 @@ package com.ordem.compra.calculocotacao;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import com.netflix.appinfo.InstanceInfo;
+/*import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.remoting.RemoteAccessException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Component;*/
 import org.springframework.web.client.RestTemplate;
 
 @Component
@@ -14,15 +14,15 @@ import org.springframework.web.client.RestTemplate;
 public class CalculoCotacaoApiClient<InstanceInfo> {
     private final static String ENDPOINT = "https://economia.awesomeapi.com.br/{moeda}/";
 
-    private final EurekaClient eurekaClient;
+    //private final EurekaClient eurekaClient;
 
     private final RestTemplate restTemplate;
 
-    public Double cotacaoMoeda(String moeda) {
+/*    public Double cotacaoMoeda(String moeda) {
         InstanceInfo apiInstanceInfo = eurekaClient.getApplication("moeda")
                 .getInstances().stream().findAny().orElseThrow(() -> new RemoteAccessException("Mesa Api indisponível"));
         return restTemplate.getForEntity(ENDPOINT, String.class, apiInstanceInfo.getHostName(), apiInstanceInfo.getPort(), id)
                 .getStatusCode()
                 .is2xxSuccessful();
-    }
+    }*/
 }
